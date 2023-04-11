c42ec965800369369b4b76144b2c741734d09b56
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-04-11 16:13:15 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace MethodSimpleExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GetName();
+        }
+
+
+        //saxeli ro sheiyvano imis metodi
+        static void GetName()
+        {
+            string name = string.Empty;
+            name = Console.ReadLine();
+            SayHi(name);
+        }
+
+
+        //sheyvanili saxeli rom dabechdos imis metodi
+        static void SayHi(string name)
+        {
+            name = ToUpperCaseFirst(name);
+            Console.WriteLine($"Welcome back {name}");
+        }
+
+
+        //pirveli aso ro didi iyos danarchenebi patara imis metodi
+        static string ToUpperCaseFirst(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return string.Empty;
+            return char.ToUpper(str[0]) + str.Substring(1).ToLower();
+
+        }
+    }
+}
