5ca0eb1032a173a88796dace65c2cbad8b09d2eb
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-04-11 11:53:09 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace MethodsPart1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d;
+            int addResult = 0;
+
+            a = 5;
+            b = 8;
+            addResult = a + b;
+            Console.WriteLine($"{a} + {b} = {addResult}");
+            DisplayMessage();
+
+            c = 15;
+            d = 10;
+            addResult = c + d;
+            Console.WriteLine($"{c} + {d} = {addResult}");
+            DisplayMessage();
+        }
+
+        static void DisplayMessage()
+        {
+            Console.WriteLine("kanec filma");
+            Console.WriteLine("Shen mohkali dzera?");
+            Console.WriteLine("date " + DateTime.Now.ToShortTimeString());
+
+            // part 2 shi davamateb axal metods
+        }
+    }
+}
