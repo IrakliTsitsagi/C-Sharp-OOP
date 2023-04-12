13e5e69e3ee5adaa87ab69b26dc07681b5b39b2e
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-04-12 12:26:53 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace MethodsPart3Value
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //out ic referensia !
+           
+          //ar gvaqvs pirveladi mnishveloba, vanichebt chven
+
+            ChangeNames(out string firstEmployee, out string secondEmployee);
+
+            Console.WriteLine($"Inside Main Method\n----------\n{firstEmployee} \n{secondEmployee}\n\n");
+        }
+
+       
+
+        static void ChangeNames(out string  firstEmp, out string secEmp)
+        {
+            firstEmp = "Olivia Aaron";
+            secEmp = "Alvaro Salazar";
+            Console.WriteLine($"Outside Main Method\n----------\n{firstEmp} \n{secEmp}\n\n");
+        }
+
+    }
+}
