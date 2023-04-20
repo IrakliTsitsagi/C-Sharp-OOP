0e5b1f531aa07e74723b41c2dae7d66b4bed7198
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-04-20 11:37:20 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace MethodsPart4OverloadedMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string guestName = "";
+            Console.WriteLine("Hello, Dear Guest, What is Your Name?");
+
+            guestName = Console.ReadLine();
+
+            if (guestName == string.Empty)
+            {
+                WelcomeGuest();
+            }
+            else
+            {
+                WelcomeGuest(guestName);
+            }
+        }
+
+        static void WelcomeGuest()
+        {
+            Console.WriteLine("Okay, we hope you anjoy your stay");
+        }
+
+        static void WelcomeGuest(string name)
+        {
+            Console.WriteLine($"Thank you, {name} we hope you anjoy your stay");
+
+        }
+    }
+}
