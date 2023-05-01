d1acddb77a484d43088cb406cdacc17a3a577225
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-05-01 14:59:03 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace StaticMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ase vidzaxebt statikuri metodidan
+            SayHi();
+
+            // ase vshvebit ara statikuridan
+            // vqmnit klaass person
+            //pabliq void uechveli ro publiq iyos
+            //shemdeg vidzaxebt ase
+            Person person = new Person();
+            person.SayHii();
+        }
+
+
+        static void SayHi()
+        {
+            Console.WriteLine("Hi! from static method");
+        }
+    }
+
+    class Person
+    {
+        public void SayHii()
+        {
+            Console.WriteLine("Hi! from non static method");
+        }
+    }
+}
