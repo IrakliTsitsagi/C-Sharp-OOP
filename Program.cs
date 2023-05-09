28e87897d2dd66d224ba81dc1083653d65bf9512
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-05-09 17:10:17 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shapes[] shapes = new Shapes[4];
+            shapes[0] = new Shapes();
+            shapes[1] = new Circles();
+            shapes[2] = new Line();
+            shapes[3] = new Triangle();
+           
+            Circles[] circle = new Circles[1];
+            circle[0] = new Circles();
+            foreach (var circlee in circle)
+            {
+                circlee.Draw();
+            }
+
+            foreach (var shape in shapes)
+            {
+                shape.Draw();
+            }
+        }
+
+        class Shapes
+        {
+            // es virtual sachiroa da shemdeg override torem otxive dawers simple shapes
+            public virtual void Draw()
+            {
+                Console.WriteLine("I'm a simple shape");
+            }
+        }
+
+        class Circles : Shapes
+        {
+            // overrides nacvlad new ro davwero isev simple shapes dawers
+            public  void Draw()
+            {
+                Console.WriteLine("I'm a circle");
+            }
+        }
+
+        class Line : Shapes
+        {
+            public override void Draw()
+            {
+                Console.WriteLine("I'm a line");
+            }
+        }
+
+        class Triangle : Shapes
+        {
+            public override void Draw()
+            {
+                Console.WriteLine("I'm a triangle");
+            }
+        }
+    }
+}
