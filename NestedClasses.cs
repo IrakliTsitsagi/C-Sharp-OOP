ee7c99fd01ab2ba782c80308444ead515c2ca94d
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-05-12 17:43:19 +0400

    Add files via upload

---
+++ NestedClasses.cs
@@ -0,0 +1,40 @@
+﻿namespace NestedClasses
+{
+    internal class NestedClasses
+    {
+        static void Main(string[] args)
+        {
+            Animals animals = new Animals();
+            // ase vshvebit nested klassis shemtxvevashi
+            Animals.Dogs dog = new Animals.Dogs();
+            Animals.Cats cat = new Animals.Cats();
+
+            // eseni rom dainaxos
+            cat.catName = "Mia";
+            dog.dogName = "Iriska";
+
+            Console.WriteLine(cat.catName + " da " + dog.dogName);
+
+
+        }
+    }
+
+    class Animals
+    {
+        public string animalName;
+        public string animalCountry;
+
+        // nested klasebia qvemot. animalsshia dogsic catsic
+        public class Dogs
+        {
+            public string dogName;
+            public string dogBreed;
+        }
+
+        public class Cats
+        {
+            public string catName;
+
+        }
+    }
+}
