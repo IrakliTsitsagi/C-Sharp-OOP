90c7cc90122a7439ad0d43080cc77bf8103a1abf
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-05-12 16:30:18 +0400

    Add files via upload

---
+++ SealedClasses.cs
@@ -0,0 +1,21 @@
+﻿namespace SealedClasses
+{
+    internal class SealedClasses
+    {
+        static void Main(string[] args)
+        {
+            Dogs dog = new Dogs();
+            // dog.name ver dainaxavs radgan sealedia. Dogs araa sealed da davinaxav dogBreeds
+        }
+    }
+
+    sealed class Animal
+    {
+        public string name;
+    }
+
+    class Dogs  // class Dogs : Animal ver vizav radgan sealedia Animali
+    {
+        public string dogBreed;
+    }
+}
