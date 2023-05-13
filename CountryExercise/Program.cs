b20084ef49fcb782742519ba51134be6bb2d75e1
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-05-13 17:46:17 +0400

    Add files via upload

---
+++ CountryExercise/Program.cs
@@ -0,0 +1,43 @@
+﻿// aq shevqmni using worldnamespaces rom martivad viyeno yvelaferi iqidan
+
+using WorldNamespace;
+
+namespace CountryExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // sheqmeni countryOneInstance da countryTwoInstance
+
+            Country countryOneInstance = new Country();
+            Country countryTwoInstance = new Country();
+
+            // sheqmeni CountryLanguages instansio
+            CountryLanguages countryLanguages = new CountryLanguages();
+
+            // mianiche ori sxvadasxva ena enumis gamoyenebit
+            countryLanguages.FirstLanguage = Languages.Georgian.ToString();
+            countryLanguages.SecondLanguage = Languages.English.ToString();
+
+            // assign the instance countryLanguages to countryOneInstance.Languages
+            // anu mivaniche es yvelaferi pirvel qveyanas
+
+            countryOneInstance.Language = countryLanguages;
+            countryOneInstance.PlanetName = "Earth";
+            countryOneInstance.Countryname = "Georgia";
+            countryOneInstance.Currency = Currencies.GEL;
+            countryOneInstance.Continents = Continents.Europe;
+
+            // gamovidzaxot sayHi pirvelic da meorec argumentiani
+            countryOneInstance.SayHi();
+            countryOneInstance.SayHi(countryOneInstance.Countryname);
+
+            Console.WriteLine($"Planet {countryOneInstance.PlanetName}, country is {countryOneInstance.Countryname} and currency is {countryOneInstance.Currency}");
+            Console.WriteLine($"Main language is {countryOneInstance.Language.FirstLanguage}, secondary language is" +
+                $" {Languages.English} and the continent is {countryOneInstance.Continents}");  // asec shemidzlia enis chawera
+
+        }
+    }
+}
---
+++ CountryExercise/World.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// sheqmeni namespace saxelad WorldNamespace
+namespace WorldNamespace
+{
+
+    // sheqmeni abstraqtuli klasi worldi
+    abstract class World
+    {
+
+        // miveci privat parametrebi 
+        private string planetName;
+        private Continents continents;
+
+        // shemdeg gavuwere propertyebi
+        public string PlanetName
+        {
+            get { return planetName; }
+            set { planetName = value; }
+        }
+
+        public Continents Continents
+        {
+            get { return continents; }
+            set { continents = value; }
+        }
+    }
+
+    //shevqmeni worldis shvili klasi country
+    class Country : World
+    {
+
+        // miveci country name, dedaqalaqi, valuta da enebi. 1 ena da 2 ena amitom structi shevqmeni qvemot
+        private string countryName;
+        private string capital;
+        private Currencies currency;
+        private CountryLanguages countryLanguages;
+
+        // gavuwerot propertyebi imitoro privatebia isini
+
+        public string Countryname
+        {
+            get { return countryName; }
+            set { countryName = value; }
+        }
+
+
+        public string Capital
+        {
+            get { return capital; }
+            set { capital = value; }
+        }
+
+        public Currencies Currency
+        {
+            get { return currency; }
+            set { currency = value; }
+        }
+
+
+        public CountryLanguages Language
+        {
+            get { return countryLanguages; }
+            set { countryLanguages = value; }
+        }
+
+
+        // shevqmeni konstruqtori qveynis saxelze da dedaqalaqze defaulti ro achvenos tu ar aqvs mnishvneloba minichebuli
+        public Country()
+        {
+            Countryname = "Unknown";
+            Capital = "Unknown";
+        }
+
+
+        // shevqmeni ori overloaded metodi erti uargumento
+        public void SayHi()
+        {
+            Console.WriteLine("Welcome There");
+        }
+
+
+        // meore erti string argumentit
+        public void SayHi(string country)
+        {
+            Console.WriteLine($"Welcome There pal from {country}");
+        }
+    }
+
+
+    // miveci 2 ena
+    struct CountryLanguages
+    {
+        private string firstLanguage;
+        private string secondLanguage;
+
+        public string FirstLanguage
+        {
+            get { return firstLanguage; }
+            set { firstLanguage = value; }
+        }
+
+        public string SecondLanguage
+        {
+            get { return secondLanguage; }
+            set { secondLanguage = value; }
+        }
+    }
+
+    // kontinentebis sia
+    enum Continents
+    {
+        Asia,
+        Africa,
+        Antarctica,
+        Australia,
+        Europe,
+        NorthAmerica,
+        SouthAmerica
+    }
+
+
+    // valutis sia
+    enum Currencies
+    {
+        GEL,
+        USD,
+        EUR,
+        RUB,
+        GBP
+    }
+
+
+    // enebis sia
+    enum Languages
+    {
+        Georgian,
+        AmericanEnglish,
+        French,
+        Russian,
+        English
+    }
+
+}
