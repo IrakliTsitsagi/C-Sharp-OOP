330d997bc5dd22c9928e55af9bd2aebf6d071b93
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-05-13 15:51:29 +0400

    Add files via upload

---
+++ Enumerations.cs
@@ -0,0 +1,31 @@
+﻿namespace Enumerations
+{
+    internal class Enumerations
+    {
+        static void Main(string[] args)
+        {
+
+            // asec sheidzleba
+            string weekDayName = WeekDay.Friday.ToString();
+
+            // asec sheidzleba
+            WeekDay day = WeekDay.Saturday;
+            Console.WriteLine(weekDayName);
+            Console.WriteLine(day);
+
+            // ase romeli dgea amovagdebt magas  meramdene dgea anu. saturday dabechdavs 5 s
+            Console.WriteLine((int)day);
+        }
+    }
+
+    enum WeekDay
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday = 60  // ase vanicheb sundays 6 is nacvlad 60 s 
+    }
+}
