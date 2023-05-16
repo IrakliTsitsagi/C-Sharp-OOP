516d7ca5d1001552634d7c04edec1579ebc63ee4
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-05-16 15:06:28 +0400

    Add files via upload

---
+++ ThisKeyword.cs
@@ -0,0 +1,34 @@
+﻿namespace ThisKeyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.DisplayName("Dejan", "Stankovic");
+        }
+    }
+
+    class Person
+    {
+        string firstName;
+        string lastName;
+
+        public void DisplayName()
+        {
+            Console.WriteLine(firstName + " " + lastName);
+            Console.WriteLine(this.firstName + " " + this.lastName); // amas moaqvs konstriqtoridan
+        }
+
+        public void DisplayName(string firstName, string lastName)
+        {
+            Console.WriteLine(firstName + " " + lastName);
+        }
+
+        public Person()
+        {
+            firstName = "not determined";
+            lastName = string.Empty;
+        }
+    }
+}
