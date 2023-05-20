4b24c77bfe865cefc75ef73c2a0b6ae5f917d420
IrakliTsitsagi <114610261+IrakliTsitsagi@users.noreply.github.com>
2023-05-20 16:11:04 +0400

    Add files via upload

---
+++ ExceptionHandling.cs
@@ -0,0 +1,37 @@
+﻿namespace ExceptionHandling
+{
+    internal class ExceptionHandling
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            // tu davwer   int x = 3; ubralod rezultats dawers da errors agar dawers radgan ar iyo erori
+            int y = 3;
+            double div = 0;
+
+            try
+            {
+                div = y / x;
+            }
+
+            // ase mainc damiwers rezultats miuxedavad imisa ro 0 ze vyof ar damaerorebs anu
+            //catch
+            //{
+            //    Console.WriteLine(div);
+            //}
+
+
+            // es damiwers errors tviton ra erroria
+            catch (Exception e)
+            {
+                Console.WriteLine("error is:" + e.Message);
+            }
+
+            // es dawers rezultats
+            finally
+            {
+                Console.WriteLine("result is:" + div);
+            }
+        }
+    }
+}
